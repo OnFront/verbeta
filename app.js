498aa2d716cccd32c7d99979ae35e3ae6271c014
Jaroslaw Decowski <62473926+OnFront@users.noreply.github.com>
2021-04-09 13:31:54 +0200

    added menuTitle el

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
 const hamburger = document.querySelector('.menu');
 const nav = document.querySelector('.navbar-nav');
-
+const menuTitle = document.querySelector('.mobile-menu-title');
 
 hamburger.addEventListener('click', function(e){
     hamburger.classList.toggle('opened');
     this.setAttribute('aria-expanded', this.classList.contains('opened'));
-    
+    menuTitle.style.display = 'block';
+
+
     nav.classList.toggle('navAnim');
     if(nav.classList.contains('navAnim')){
         nav.style.display = 'flex';
     } else {
         nav.style.display = 'none';
+        menuTitle.style.display = 'none';
     }
 });
 
